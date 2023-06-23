5932502634ed8362cdd3ce3e0feaf99a196fde2d
jangth0655 <jangth0655@gmail.com>
2023-06-24 00:57:42 +0900

    refact : postItem - 스타일링을 위해 useRouter로 업데이트

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -2,32 +2,46 @@ import { Post } from '@/types/post';
 import { dateFormat, formatMMMdYYYt } from '@/utils/dateFormat';
 import { AiOutlineArrowRight } from 'react-icons/ai';
 import Link from 'next/link';
+import { useCallback } from 'react';
+import { useRouter } from 'next/navigation';
 
 type Props = {
   post: Post;
 };
 
 export default function PostItem({ post }: Props) {
+  const router = useRouter();
   const { category, date, description, featured, path, title } = post;
 
+  const handleRoute = useCallback(() => {
+    router.push(`/posts/${path}`);
+  }, [path, router]);
+
   return (
-    <li className='mb-16 last:mb-0 xl:flex xl:flex-row items-start'>
+    <li className='mb-16 last:mb-0 xl:flex items-start'>
       <div className='hidden xl:block mr-48 mt-2 '>
         <span className='text-gray-500 text-md'>{formatMMMdYYYt(date)}</span>
       </div>
       <div>
-        <Link href={`/posts/${path}`} as={`/posts/${path}`}>
+        <div className='flex flex-col'>
+          <span className='text-gray-500 xl:hidden mb-3'>
+            {dateFormat(date)}
+          </span>
           <div>
-            <span className='text-gray-500 xl:hidden inline-block mb-3'>
-              {dateFormat(date)}
-            </span>
-            <h1 className='text-neutral-800 font-bold text-2xl'>{title}</h1>
-            <span className='text-teal-500 font-thin hover:text-teal-600 transition-all'>
-              {category}
+            <span
+              onClick={handleRoute}
+              className='text-neutral-800 font-bold text-2xl cursor-pointer'
+            >
+              {title}
             </span>
           </div>
-        </Link>
-        <p className='text-neutral-400 mt-6'>{description}</p>
+          <span className='text-teal-500 font-thin hover:text-teal-600 transition-all'>
+            {category}
+          </span>
+        </div>
+        <p className='text-neutral-400 mt-6 text-ellipsis whitespace-nowrap overflow-hidden'>
+          {description}
+        </p>
         <div className='mt-10 text-teal-500'>
           <Link
             href={`/posts/${path}`}
