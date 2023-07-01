bf6097cc35750a9f0a1f5a1e281979400771017f
jangth0655 <jangth0655@gmail.com>
2023-07-01 10:07:40 +0900

    feat : 검색 기능 업데이트

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -11,26 +11,45 @@ type Props = {
   total: number;
 };
 
+export type Form = {
+  keyword?: string;
+};
+
 export default function PostList({ posts, total }: Props) {
   const [page, setPage] = useState(FIRST_PAGE);
   const maxPage = Math.ceil(total / OFFSET);
   const headerRef = useRef<HTMLDivElement>(null);
+  const [searchValue, seetSearchValue] = useState<Form>({
+    keyword: undefined,
+  });
 
   const moveTop = () => {
     headerRef.current?.scrollIntoView({ behavior: 'smooth', block: 'end' });
   };
 
+  const handleSearchValue = (keyword: string) => {
+    seetSearchValue({
+      keyword,
+    });
+  };
+
+  const searchPosts =
+    searchValue.keyword &&
+    posts.data.filter((post) => post.title.includes(searchValue.keyword || ''));
+
+  const filterPosts = searchPosts || posts.data;
+
   return (
     <section>
       <header ref={headerRef} className='mt-12'>
         <h1 className='font-bold text-4xl xl:text-6xl'>All Posts</h1>
-        <SearchForm />
+        <SearchForm handleSearchValue={handleSearchValue} />
       </header>
 
       <div className='w-full h-[0.8px] dark:bg-gray-600 bg-gray-300 my-20' />
 
       <ul className='min-h-screen'>
-        {posts?.data
+        {filterPosts
           .slice(page * OFFSET, OFFSET + page * OFFSET)
           .map((post) => (
             <PostItem key={post.path} post={post} />
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,12 +1,33 @@
 'use client';
 
+import { ChangeEvent, FormEvent, useCallback, useState } from 'react';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 
-export default function SearchForm() {
+type Props = {
+  handleSearchValue: (keyword: string) => void;
+};
+
+export default function SearchForm({ handleSearchValue }: Props) {
+  const [keyword, setKeyword] = useState<string>('');
+
+  const handleChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const keyword = e.target.value;
+    setKeyword(keyword);
+  }, []);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!handleSearchValue) return;
+    handleSearchValue(keyword);
+    setKeyword('');
+  };
+
   return (
-    <form className='mt-4'>
+    <form onSubmit={onSubmit} className='mt-4'>
       <div className='relative border-2 rounded-lg w-3/4 md:w-3/5'>
         <input
+          value={keyword}
+          onChange={handleChange}
           className='px-4 py-2 w-full outline-none dark:text-black dark:bg-white dark:border-0'
           placeholder='Search article'
           type='text'
