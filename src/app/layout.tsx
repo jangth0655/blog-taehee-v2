38507d04b557e4481169b6eb9b0922b3cf6fdb0e
jangth0655 <jangth0655@gmail.com>
2023-07-02 10:37:30 +0900

    feat : Projects 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import './globals.css';
-import { Noto_Sans } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import Navbar from '@/components/navbar/Navbar';
 import DarkThemeProvider from '@/ThemeProvider';
 import { NavbarContextProvider } from '@/context/NavbarContext';
@@ -8,7 +8,7 @@ type Props = {
   children: React.ReactNode;
 };
 
-const notoSans = Noto_Sans({
+const inter = Inter({
   weight: ['400', '500', '600', '800'],
   subsets: ['latin'],
   display: 'swap',
@@ -16,7 +16,7 @@ const notoSans = Noto_Sans({
 
 const Layout = ({ children }: Props) => {
   return (
-    <html className={notoSans.className} suppressHydrationWarning>
+    <html className={inter.className} suppressHydrationWarning>
       <body className='max-w-[640px] px-6 sm:max-w-3xl xl:max-w-5xl m-auto'>
         <DarkThemeProvider>
           <NavbarContextProvider>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,41 @@
+import Link from 'next/link';
 import Header from '@/components/Header';
+import { AiOutlineArrowRight } from 'react-icons/ai';
+
+const testData = [
+  {
+    title: 'test',
+    path: '#',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ea corporis repudiandae culpa itaque. Nesciunt consequuntur recusandae ipsum qui maxime, doloremque illo laudantium blanditiis ratione? Praesentium reprehenderit dignissimos rerum veniam?',
+    category: 'test category',
+    key: '1',
+  },
+  {
+    title: 'test1',
+    path: '#1',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ea corporis repudiandae culpa itaque. Nesciunt consequuntur recusandae ipsum qui maxime, doloremque illo laudantium blanditiis ratione? Praesentium reprehenderit dignissimos rerum veniam?',
+    category: 'test category1',
+    key: '2',
+  },
+  {
+    title: 'test2',
+    path: '#2',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ea corporis repudiandae culpa itaque. Nesciunt consequuntur recusandae ipsum qui maxime, doloremque illo laudantium blanditiis ratione? Praesentium reprehenderit dignissimos rerum veniam?',
+    category: 'test category2',
+    key: '3',
+  },
+  {
+    title: 'test3',
+    path: '#3',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ea corporis repudiandae culpa itaque. Nesciunt consequuntur recusandae ipsum qui maxime, doloremque illo laudantium blanditiis ratione? Praesentium reprehenderit dignissimos rerum veniam?',
+    category: 'test category3',
+    key: '4',
+  },
+];
 
 export default function ProjectsPage() {
   return (
@@ -6,6 +43,37 @@ export default function ProjectsPage() {
       <Header headerTitle='Projects'>
         <div className='w-full h-[0.8px] dark:bg-gray-600 bg-gray-300 mt-10' />
       </Header>
+
+      <ul className='grid md:grid-cols-2 grid-cols-1 gap-8 w-[32rem] md:w-full  h-full'>
+        {testData.map((test) => (
+          <li
+            key={test.key}
+            className='border-2 border-gray-200 dark:border-gray-700 p-4 rounded-md space-y-4'
+          >
+            <div>
+              <div className='w-full h-80 md:h-60 bg-slate-700' />
+            </div>
+            <div className='space-y-4'>
+              <div>
+                <span className='text-xl font-bold'>{test.title}</span>
+                <p className='mt-2 text-gray-500 dark:text-gray-400'>
+                  {test.description}
+                </p>
+              </div>
+              <div className='text-teal-500'>
+                <Link
+                  href={`#`}
+                  as=''
+                  className='flex items-center hover:text-teal-600 transition-all dark:hover:text-teal-300'
+                >
+                  <span className='inline-block mr-2'>Read more </span>
+                  <AiOutlineArrowRight />
+                </Link>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
