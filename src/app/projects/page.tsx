04e1b7379af722483a0410478736c146e0ef97d9
jangth0655 <jangth0655@gmail.com>
2023-07-07 22:56:59 +0900

    feat : 프로젝트 리스트 비어있을 경우 텍스트 업데이트

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,12 +4,21 @@ import { getProjects } from '@/service/projects';
 
 export default async function ProjectsPage() {
   const projects = await getProjects();
+  const hasNotProjects = projects.total === 0 || projects.data.length === 0;
   return (
     <section>
       <Header headerTitle='Projects'>
         <div className='w-full h-[0.8px] dark:bg-gray-600 bg-gray-300 mt-10' />
       </Header>
-      <ProjectList projects={projects} total={projects.total} />
+      {hasNotProjects ? (
+        <div>
+          <p className='text-neutral-300 text-xl'>
+            현재 프로젝트 업데이트 중입니다. 🔥🔥🔥
+          </p>
+        </div>
+      ) : (
+        <ProjectList projects={projects} total={projects.total} />
+      )}
     </section>
   );
 }
