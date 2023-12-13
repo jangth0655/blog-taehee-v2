5efdf99da2536d3113ddec5f6919c05b1050e33c
jangth0655 <jangth0655@gmail.com>
2023-12-13 11:38:50 +0900

    post, layout UI업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,27 @@
-import './globals.css';
-import { Montserrat } from 'next/font/google';
-import Navbar from '@/components/navbar/Navbar';
-import DarkThemeProvider from '@/ThemeProvider';
-import { NavbarContextProvider } from '@/context/NavbarContext';
-import { Metadata } from 'next';
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+import Navbar from "@/components/navbar/Navbar";
+import DarkThemeProvider from "@/ThemeProvider";
+import { NavbarContextProvider } from "@/context/NavbarContext";
+import { Metadata } from "next";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: {
-    default: 'taehee blog',
-    template: 'taehee blog | %s',
+    default: "taehee blog",
+    template: "taehee blog | %s",
   },
-  description: '프론트엔드 개발자 taehee blog',
+  description: "프론트엔드 개발자 taehee blog",
   icons: {
-    icon: '/favicon.ico',
+    icon: "/favicon.ico",
   },
 };
 
--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,10 +1,10 @@
-import { Post } from '@/types/post';
-import { dateFormat, formatMMMdYYYt } from '@/utils/dateFormat';
-import { AiOutlineArrowRight } from 'react-icons/ai';
-import Link from 'next/link';
-import { useCallback } from 'react';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
+import { Post } from "@/types/post";
+import { formatMMMdYYYt } from "@/utils/dateFormat";
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Link from "next/link";
+import { useCallback } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 type Props = {
   post: Post;
@@ -24,48 +24,46 @@ export default function PostItem({ post }: Props) {
   const sliceDescription = description.slice(0, 110);
 
   return (
-    <li className='mb-16 last:mb-0 xl:flex items-start'>
-      <div className='hidden xl:block mr-48 mt-2 w-[30%] text-center px-1 '>
-        <span className='text-gray-400 text-md'>{formatMMMdYYYt(date)}</span>
+    <li className="mb-16 last:mb-0 xl:flex items-start justify-between space-y-2 xl:space-y-0 xl:space-x-12">
+      <div className="mt-2 px-1 mb-2 md:mb-0 flex md:w-[30%]">
+        <span className="text-gray-400 text-md">{formatMMMdYYYt(date)}</span>
       </div>
-      <div className='w-[70%]'>
-        <div className='flex flex-col'>
-          <span className='text-gray-400 xl:hidden mb-3'>
-            {dateFormat(date)}
-          </span>
+
+      <div className="w-[60%]">
+        <div className="flex flex-col">
           <div>
             <span
               onClick={handleRoute}
-              className='text-neutral-800 font-bold text-2xl cursor-pointer dark:text-white'
+              className="text-neutral-800 font-bold text-2xl cursor-pointer dark:text-white"
             >
               {title}
             </span>
           </div>
-          <span className='text-emerald-600 opacity-80'>{category}</span>
+          <span className="text-emerald-600 opacity-80">{category}</span>
         </div>
         {post.image && (
-          <div className='my-2 relative w-96 h-64'>
+          <div className="my-2 relative w-96 h-64">
             <Image
               src={post.image}
               fill
-              alt='blog'
-              style={{ borderRadius: 8, objectFit: 'cover' }}
+              alt="blog"
+              style={{ borderRadius: 8, objectFit: "cover" }}
               priority
             />
           </div>
         )}
-        <p className='text-gray-500 dark:text-gray-300 mt-6 text-ellipsis overflow-hidden '>
+        <p className="text-gray-500 dark:text-gray-300 mt-6 text-ellipsis overflow-hidden ">
           {sliceDescriptionLength < STANDARD_LENGTH
             ? sliceDescription
             : `${sliceDescription}...`}
         </p>
-        <div className='mt-2 dark:text-teal-500 text-teal-600 font-bold'>
+        <div className="mt-2 dark:text-teal-500 text-teal-600 font-bold">
           <Link
             href={`/posts/${path}`}
             as={`/posts/${path}`}
-            className='flex items-center  hover:text-teal-500 transition-all dark:hover:text-teal-300'
+            className="flex items-center  hover:text-teal-500 transition-all dark:hover:text-teal-300"
           >
-            <span className='inline-block mr-2'>Read more </span>
+            <span className="inline-block mr-2">Read more </span>
             <AiOutlineArrowRight />
           </Link>
         </div>
